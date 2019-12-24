af288e1543b8da33bbfa33756ba469bb66e6c691
Dhrumil Patel <dhrumilp15@gmail.com>
2019-12-24 12:11:36 -0500

    quiqui, do you love me...

---
+++ quiqui/lib/assets/images.dart
@@ -0,0 +1,87 @@
+Map<String, dynamic> json =
+'''
+{
+  "dogs" : [
+    {
+      "name" : "Toto",
+      "filepath" : "images/toto.jpg"
+    },
+    {
+      "name" : "Laika",
+      "filepath" : "images/laika.jpg"
+    },
+    {
+      "name" : "Lassie",
+      "filepath" : "images/lassie.jpg"
+    },
+    {
+      "name" : "Scooby",
+      "filepath" : "images/scooby.jpg"
+    },
+    {
+      "name" : "Balto",
+      "filepath" : "images/balto.jpg"
+    },
+    {
+      "name" : "Bo",
+      "filepath" : "images/bo.jpg"
+    },
+    {
+      "name" : "Sunny",
+      "filepath" : "images/sunny.jpg"
+    },
+    {
+      "name" : "Higgins",
+      "filepath" : "images/higgins.jpg"
+    },
+    {
+      "name" : "Doug",
+      "filepath" : "images/doug.jpg"
+    },
+    {
+      "name" : "Wishbone",
+      "filepath" : "images/wishbone.jpg"
+    },
+    {
+      "name" : "Rin Tin Tin",
+      "filepath" : "images/rintintin.jpg"
+    },
+    {
+      "name" : "Old Yeller",
+      "filepath" : "images/oldyeller.jpg"
+    },
+    {
+      "name" : "Sinbad",
+      "filepath" : "images/sinbad.jpg"
+    },
+    {
+      "name" : "Trakr",
+      "filepath" : "images/trakr.jpg"
+    },
+    {
+      "name" : "Bobbie",
+      "filepath" : "images/bobbie.jpg"
+    },
+    {
+      "name" : "Jofi",
+      "filepath" : "images/jofi.jpg"
+    },
+    {
+      "name" : "Koko",
+      "filepath" : "images/koko.jpeg"
+    },
+    {
+      "name" : "Snowy",
+      "filepath" : "images/snowy.jpg"
+    },
+    {
+      "name" : "Spot",
+      "filepath" : "images/spot.jpg"
+    },
+    {
+      "name" : "Milie",
+      "filepath" : "images/milie.jpg"
+    }
+  ]
+}
+'''
